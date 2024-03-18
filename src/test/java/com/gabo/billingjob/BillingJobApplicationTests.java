6bec257b8c3c72a0b1e43757e2f40c075443ad9b
Gabriel Pereyra <gabrielp1689@gmail.com>
2024-03-17 21:32:16 -0300

    init project

---
+++ src/test/java/com/gabo/billingjob/BillingJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabo.billingjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
