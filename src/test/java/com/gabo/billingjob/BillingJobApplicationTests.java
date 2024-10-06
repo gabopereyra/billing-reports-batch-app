57ceeb42cc19eaa87217dca62f53eec83c160ffc
Gabriel Pereyra <gabrielp1689@gmail.com>
2024-10-06 10:44:34 -0300

    add step example and update test

--- src/test/java/com/gabo/billingjob/BillingJobApplicationTests.java
+++ src/test/java/com/gabo/billingjob/BillingJobApplicationTests.java
@@ -3,19 +3,21 @@ package com.gabo.billingjob;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.batch.core.*;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.system.CapturedOutput;
-import org.springframework.boot.test.system.OutputCaptureExtension;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @SpringBatchTest
 @SpringBootTest
-@ExtendWith(OutputCaptureExtension.class)
 class BillingJobApplicationTests {
 	@Autowired
 	private JobLauncherTestUtils jobLauncherTestUtils;
@@ -29,18 +31,17 @@ class BillingJobApplicationTests {
 	}
 
 	@Test
-	void testJobExecution(CapturedOutput output) throws Exception {
+	void testJobExecution() throws Exception {
 		// given
-		JobParameters jobParameters = this.jobLauncherTestUtils.getUniqueJobParametersBuilder()
-				.addString("input.file", "/some/input/file")
+		JobParameters jobParameters = new JobParametersBuilder()
+				.addString("input.file", "src/main/resources/billing-2023-01.csv")
 				.toJobParameters();
 
 		// when
-		// ** Update the following line:
 		JobExecution jobExecution = this.jobLauncherTestUtils.launchJob(jobParameters);
 
 		// then
-		Assertions.assertTrue(output.getOut().contains("processing billing information from file /some/input/file"));
 		Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+		Assertions.assertTrue(Files.exists(Paths.get("staging", "billing-2023-01.csv")));
 	}
 }
